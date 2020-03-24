157999d0176a68bdacf394be46062e5684a02cfb
GiovanniHenriksen <giovannihenriksen@users.noreply.github.com>
2020-03-24 16:14:05 +0100

    Styling changes for the chart: - DarkMode: Tooltip box is now white - Point radius will increase to '7' when the mouse is hovering over the chart

--- src/anki_simulator/gui/graph.py
+++ src/anki_simulator/gui/graph.py
@@ -21,7 +21,6 @@ def json_serial(obj):
 
 
 class Graph(AnkiWebView):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__controls()
@@ -29,18 +28,30 @@ class Graph(AnkiWebView):
     def __controls(self):
         path = os.path.join(parent_dir, "graph.html")
         pathChart = os.path.join(parent_dir, "chart/dist/Chart.bundle.min.js")
-        html = "<head><meta charset=\"UTF-8\"><script>{}</script>".format(open(pathChart, 'r').read())
-        
+        html = '<head><meta charset="UTF-8"><script>{}</script>'.format(
+            open(pathChart, "r").read()
+        )
+
         if theme_manager and theme_manager.night_mode:
             html += "<style>#chart{background-color:black;}</style>"
-            html += "<script>Chart.defaults.global.defaultFontColor = 'white';" \
-                    "Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.2)';" \
-                    "Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.25)';</script>"
-        html += open(path, 'r').read()
+            html += (
+                "<script>"
+                "Chart.defaults.global.defaultFontColor = 'white';"
+                "Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.2)';"
+                "Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.25)';"
+                "Chart.defaults.global.tooltips.backgroundColor = 'rgba(255, 255, 255, 0.9)';"
+                "Chart.defaults.global.tooltips.bodyFontColor = 'rgba(0, 0, 0, 1)';"
+                "Chart.defaults.global.tooltips.titleFontColor = 'rgba(0, 0, 0, 1)';"
+                "Chart.defaults.global.tooltips.footerFontColor = 'rgba(0, 0, 0, 1)';"
+                "</script>"
+            )
+        html += open(path, "r").read()
         self.setHtml(html)
 
     def addDataSet(self, label, set):
-        self.eval("newDataSet('{}', '{}')".format(label, json.dumps(set, default=json_serial)))
+        self.eval(
+            "newDataSet('{}', '{}')".format(label, json.dumps(set, default=json_serial))
+        )
 
     def clearLastDataset(self):
         self.eval("clearLastDataset()")
