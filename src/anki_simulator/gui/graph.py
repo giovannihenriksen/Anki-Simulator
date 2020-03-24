a21f7ccd6e1b75bd17952bf8d2fce1ccc5ba0894
Glutanimate <glutanimate@users.noreply.github.com>
2020-03-24 15:56:39 +0100

    Also run black on graph.py

--- src/anki_simulator/gui/graph.py
+++ src/anki_simulator/gui/graph.py
@@ -21,7 +21,6 @@ def json_serial(obj):
 
 
 class Graph(AnkiWebView):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__controls()
@@ -29,18 +28,24 @@ class Graph(AnkiWebView):
     def __controls(self):
         path = os.path.join(parent_dir, "graph.html")
         pathChart = os.path.join(parent_dir, "chart/dist/Chart.bundle.min.js")
-        html = "<head><meta charset=\"UTF-8\"><script>{}</script>".format(open(pathChart, 'r').read())
-        
+        html = '<head><meta charset="UTF-8"><script>{}</script>'.format(
+            open(pathChart, "r").read()
+        )
+
         if theme_manager and theme_manager.night_mode:
             html += "<style>#chart{background-color:black;}</style>"
-            html += "<script>Chart.defaults.global.defaultFontColor = 'white';" \
-                    "Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.2)';" \
-                    "Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.25)';</script>"
-        html += open(path, 'r').read()
+            html += (
+                "<script>Chart.defaults.global.defaultFontColor = 'white';"
+                "Chart.defaults.scale.gridLines.color = 'rgba(255, 255, 255, 0.2)';"
+                "Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.25)';</script>"
+            )
+        html += open(path, "r").read()
         self.setHtml(html)
 
     def addDataSet(self, label, set):
-        self.eval("newDataSet('{}', '{}')".format(label, json.dumps(set, default=json_serial)))
+        self.eval(
+            "newDataSet('{}', '{}')".format(label, json.dumps(set, default=json_serial))
+        )
 
     def clearLastDataset(self):
         self.eval("clearLastDataset()")
