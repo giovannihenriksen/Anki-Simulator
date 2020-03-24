ba0c99e1ecf854d4cf11a2118fa2803228276bb9
GiovanniHenriksen <noreply@example.com>
2020-03-24 15:49:45 +0100

    Code reformatted using Black

--- src/anki_simulator/__init__.py
+++ src/anki_simulator/__init__.py
@@ -27,7 +27,6 @@ def stepsAreValid(steps):
 
 
 class SimulatorDialog(QDialog):
-
     def __init__(self, mw, deck_id=None):
         QDialog.__init__(self, parent=mw)
         self.mw = mw
@@ -40,8 +39,12 @@ class SimulatorDialog(QDialog):
             if deck_name:
                 self.deckChooser.setDeckName(deck_name)
         self.dialog.simulateButton.clicked.connect(self.simulate)
-        self.dialog.loadDeckConfigurationsButton.clicked.connect(self.loadDeckConfigurations)
-        self.dialog.clearLastSimulationButton.clicked.connect(self.clear_last_simulation)
+        self.dialog.loadDeckConfigurationsButton.clicked.connect(
+            self.loadDeckConfigurations
+        )
+        self.dialog.clearLastSimulationButton.clicked.connect(
+            self.clear_last_simulation
+        )
         self.loadDeckConfigurations()
         self.numberOfSimulations = 0
         restoreGeom(self, "simulatorDialog")
@@ -67,35 +70,48 @@ class SimulatorDialog(QDialog):
     def loadDeckConfigurations(self):
         deckID = self.deckChooser.selectedId()
         conf = self.mw.col.decks.confForDid(deckID)
-        numberOfNewCardsPerDay = conf['new']['perDay']
-        startingEase = conf['new']['initialFactor'] / 10.0
-        intervalModifier = conf['rev']['ivlFct'] * 100
-        maxRevPerDay = conf['rev']['perDay']
-        numberOfLearningSteps = len(conf['new']['delays'])
-        learningSteps = listToUser(conf['new']['delays'])
-        numberOfLapseSteps = len(conf['lapse']['delays'])
-        lapseSteps = listToUser(conf['lapse']['delays'])
-        graduatingInterval = conf['new']['ints'][0]
-        newLapseInterval = conf['lapse']['mult'] * 100
-        maxInterval = conf['rev']['maxIvl']
+        numberOfNewCardsPerDay = conf["new"]["perDay"]
+        startingEase = conf["new"]["initialFactor"] / 10.0
+        intervalModifier = conf["rev"]["ivlFct"] * 100
+        maxRevPerDay = conf["rev"]["perDay"]
+        numberOfLearningSteps = len(conf["new"]["delays"])
+        learningSteps = listToUser(conf["new"]["delays"])
+        numberOfLapseSteps = len(conf["lapse"]["delays"])
+        lapseSteps = listToUser(conf["lapse"]["delays"])
+        graduatingInterval = conf["new"]["ints"][0]
+        newLapseInterval = conf["lapse"]["mult"] * 100
+        maxInterval = conf["rev"]["maxIvl"]
 
-        self.dialog.newCardsPerDaySpinbox.setProperty('value', numberOfNewCardsPerDay)
-        self.dialog.startingEaseSpinBox.setProperty('value', startingEase)
-        self.dialog.intervalModifierSpinbox.setProperty('value', intervalModifier)
-        self.dialog.maximumReviewsPerDaySpinbox.setProperty('value', maxRevPerDay)
+        self.dialog.newCardsPerDaySpinbox.setProperty("value", numberOfNewCardsPerDay)
+        self.dialog.startingEaseSpinBox.setProperty("value", startingEase)
+        self.dialog.intervalModifierSpinbox.setProperty("value", intervalModifier)
+        self.dialog.maximumReviewsPerDaySpinbox.setProperty("value", maxRevPerDay)
         self.dialog.learningStepsTextfield.setText(learningSteps)
         self.dialog.lapseStepsTextfield.setText(lapseSteps)
-        self.dialog.graduatingIntervalSpinbox.setProperty('value', graduatingInterval)
-        self.dialog.newLapseIntervalSpinbox.setProperty('value', newLapseInterval)
-        self.dialog.maximumIntervalSpinbox.setProperty('value', maxInterval)
-        self.dialog.percentCorrectLearningTextfield.setText(listToUser([92] * numberOfLearningSteps))
-        self.dialog.percentCorrectLapseTextfield.setText(listToUser([92] * numberOfLapseSteps))
+        self.dialog.graduatingIntervalSpinbox.setProperty("value", graduatingInterval)
+        self.dialog.newLapseIntervalSpinbox.setProperty("value", newLapseInterval)
+        self.dialog.maximumIntervalSpinbox.setProperty("value", maxInterval)
+        self.dialog.percentCorrectLearningTextfield.setText(
+            listToUser([92] * numberOfLearningSteps)
+        )
+        self.dialog.percentCorrectLapseTextfield.setText(
+            listToUser([92] * numberOfLapseSteps)
+        )
 
-    def loadCards(self, did, days_to_simulate, number_of_new_cards_per_day, starting_ease, number_of_learning_steps,
-                  number_of_lapse_steps, include_overdue_cards):
+    def loadCards(
+        self,
+        did,
+        days_to_simulate,
+        number_of_new_cards_per_day,
+        starting_ease,
+        number_of_learning_steps,
+        number_of_lapse_steps,
+        include_overdue_cards,
+    ):
         # Before we start the simulation, we will collect all the cards from the database.
         crt = date.fromtimestamp(
-            self.mw.col.crt)  # Gets collection creation time. We need this to find out when a card is due.
+            self.mw.col.crt
+        )  # Gets collection creation time. We need this to find out when a card is due.
         today = date.today()
         todayInteger = (today - crt).days
         dateArray = []
@@ -107,7 +123,14 @@ class SimulatorDialog(QDialog):
             card = self.mw.col.getCard(cid)
             if card.type == 0:
                 # New card
-                review = dict(id=card.id, ease=starting_ease, state='unseen', step=0, reviews=[], delay=0)
+                review = dict(
+                    id=card.id,
+                    ease=starting_ease,
+                    state="unseen",
+                    step=0,
+                    reviews=[],
+                    delay=0,
+                )
                 newCards.append(review)
             elif card.type == 1:
                 # Learning card
@@ -126,8 +149,14 @@ class SimulatorDialog(QDialog):
                     else:
                         # Card is overdue. We will not include it in the simulation.
                         continue
-                review = dict(id=card.id, ease=starting_ease, state='learning',
-                              step=max(number_of_learning_steps - (card.left % 10), -1), reviews=[], delay=0)
+                review = dict(
+                    id=card.id,
+                    ease=starting_ease,
+                    state="learning",
+                    step=max(number_of_learning_steps - (card.left % 10), -1),
+                    reviews=[],
+                    delay=0,
+                )
                 if cardDue < days_to_simulate:
                     dateArray[cardDue].append(review)
             elif card.type == 2:
@@ -145,12 +174,14 @@ class SimulatorDialog(QDialog):
                     else:
                         # Card is overdue. We will not include it in the simulation.
                         continue
-                review = dict(id=card.id, ease=card.factor / 10, currentInterval=card.ivl, delay=0)
+                review = dict(
+                    id=card.id, ease=card.factor / 10, currentInterval=card.ivl, delay=0
+                )
                 if card.ivl >= 21:
-                    review['state'] = 'mature'
+                    review["state"] = "mature"
                 else:
-                    review['state'] = 'young'
-                review['reviews'] = []
+                    review["state"] = "young"
+                review["reviews"] = []
                 if cardDue < days_to_simulate:
                     dateArray[cardDue].append(review)
             elif card.type == 3:
@@ -170,42 +201,65 @@ class SimulatorDialog(QDialog):
                     else:
                         # Card is overdue. We will not include it in the simulation.
                         continue
-                review = dict(id=card.id, ease=card.factor / 10, state='relearn', currentInterval=card.ivl,
-                              step=max(number_of_lapse_steps - (
-                                      card.left % 10), -1), reviews=[], delay=0)
+                review = dict(
+                    id=card.id,
+                    ease=card.factor / 10,
+                    state="relearn",
+                    currentInterval=card.ivl,
+                    step=max(number_of_lapse_steps - (card.left % 10), -1),
+                    reviews=[],
+                    delay=0,
+                )
                 if cardDue < days_to_simulate:
                     dateArray[cardDue].append(review)
 
         # Adding new cards
         if number_of_new_cards_per_day > 0:
             deck = self.mw.col.decks.get(did)
-            newCardsAlreadySeenToday = min(deck['newToday'][1], number_of_new_cards_per_day)
+            newCardsAlreadySeenToday = min(
+                deck["newToday"][1], number_of_new_cards_per_day
+            )
             for index, card in enumerate(newCards):
-                dayToAddNewCardsTo = int((index + newCardsAlreadySeenToday) / number_of_new_cards_per_day)
+                dayToAddNewCardsTo = int(
+                    (index + newCardsAlreadySeenToday) / number_of_new_cards_per_day
+                )
                 if dayToAddNewCardsTo < days_to_simulate:
                     dateArray[dayToAddNewCardsTo].append(card)
         return dateArray
 
-    def createSimulatedDateArray(self, days_to_simulate, number_of_new_cards_per_day, new_cards_in_deck, starting_ease):
+    def createSimulatedDateArray(
+        self,
+        days_to_simulate,
+        number_of_new_cards_per_day,
+        new_cards_in_deck,
+        starting_ease,
+    ):
         cards_left = new_cards_in_deck
         dateArray = []
         for day in range(days_to_simulate):
             if not cards_left:
                 dateArray.append([])
                 continue
-                            
+
             cards_for_the_day = []
             left_today = min(number_of_new_cards_per_day, cards_left)
-            
+
             for cid in range(left_today):
                 cards_for_the_day.append(
-                    dict(id=cid, ease=starting_ease, state='unseen', step=0, reviews=[], delay=0)
+                    dict(
+                        id=cid,
+                        ease=starting_ease,
+                        state="unseen",
+                        step=0,
+                        reviews=[],
+                        delay=0,
+                    )
                 )
-            
+
             dateArray.append(cards_for_the_day)
-            
+
             cards_left -= left_today
-        
+
         return dateArray
 
     def simulate(self):
@@ -215,12 +269,14 @@ class SimulatorDialog(QDialog):
         intervalModifier = float(self.dialog.intervalModifierSpinbox.value()) / 100
         maxReviewsPerDay = int(self.dialog.maximumReviewsPerDaySpinbox.value())
         if not stepsAreValid(self.dialog.learningStepsTextfield.text().split()):
-            showInfo('Please correctly enter \'Learning steps\' (e.g. \'30 1440\')')
+            showInfo("Please correctly enter 'Learning steps' (e.g. '30 1440')")
             self.dialog.learningStepsTextfield.setFocus()
             return
-        learningSteps = [int(i) for i in self.dialog.learningStepsTextfield.text().split()]
+        learningSteps = [
+            int(i) for i in self.dialog.learningStepsTextfield.text().split()
+        ]
         if not stepsAreValid(self.dialog.lapseStepsTextfield.text().split()):
-            showInfo('Please correctly enter \'Lapse steps\' (e.g. \'30 1440\')')
+            showInfo("Please correctly enter 'Lapse steps' (e.g. '30 1440')")
             self.dialog.lapseStepsTextfield.setFocus()
             return
         lapseSteps = [int(i) for i in self.dialog.lapseStepsTextfield.text().split()]
@@ -228,49 +284,79 @@ class SimulatorDialog(QDialog):
         newLapseInterval = float(self.dialog.newLapseIntervalSpinbox.value()) / 100
         maxInterval = int(self.dialog.maximumIntervalSpinbox.value())
         chanceRightUnseen = float(self.dialog.percentCorrectUnseenSpinbox.value()) / 100
-        if not stepsAreValid(self.dialog.percentCorrectLearningTextfield.text().split()):
-            showInfo('Please correctly enter \'% correct learning steps\' (e.g. \'90 90\')')
+        if not stepsAreValid(
+            self.dialog.percentCorrectLearningTextfield.text().split()
+        ):
+            showInfo("Please correctly enter '% correct learning steps' (e.g. '90 90')")
             self.dialog.percentCorrectLearningTextfield.setFocus()
             return
-        percentagesCorrectForLearningSteps = [float(i) / 100 for i in
-                                              self.dialog.percentCorrectLearningTextfield.text().split()]
+        percentagesCorrectForLearningSteps = [
+            float(i) / 100
+            for i in self.dialog.percentCorrectLearningTextfield.text().split()
+        ]
         if len(percentagesCorrectForLearningSteps) != len(learningSteps):
-            showInfo("Number of \'% correct learning steps\' does not match the number of \'Learning steps\'")
+            showInfo(
+                "Number of '% correct learning steps' does not match the number of 'Learning steps'"
+            )
             self.dialog.percentCorrectLearningTextfield.setFocus()
             return
         if not stepsAreValid(self.dialog.percentCorrectLapseTextfield.text().split()):
-            showInfo('Please correctly enter \'% correct lapse steps\' (e.g. \'90 90\')')
+            showInfo("Please correctly enter '% correct lapse steps' (e.g. '90 90')")
             self.dialog.percentCorrectLapseTextfield.setFocus()
             return
-        percentagesCorrectForLapseSteps = [float(i) / 100 for i in
-                                           self.dialog.percentCorrectLapseTextfield.text().split()]
+        percentagesCorrectForLapseSteps = [
+            float(i) / 100
+            for i in self.dialog.percentCorrectLapseTextfield.text().split()
+        ]
         if len(percentagesCorrectForLapseSteps) != len(lapseSteps):
-            showInfo("Number of \'% correct lapse steps\' does not match the number of \'Lapse steps\'")
+            showInfo(
+                "Number of '% correct lapse steps' does not match the number of 'Lapse steps'"
+            )
             self.dialog.percentCorrectLapseTextfield.setFocus()
             return
         chanceRightYoung = float(self.dialog.percentCorrectYoungSpinbox.value()) / 100
         chanceRightMature = float(self.dialog.percentCorrectMatureSpinbox.value()) / 100
-        
+
         mockCardState = self.dialog.mockedCardStateRadio.isChecked()
         mockedNewCards = self.dialog.mockedNewCardsSpinbox.value()
-        
+
         if not mockCardState:
             # Use actual card data for simulation
             includeOverdueCards = self.dialog.includeOverdueCardsCheckbox.isChecked()
             # returns an array of days, each day is another array that contains all
             # the cards for that day:
-            dateArray = self.loadCards(self.deckChooser.selectedId(), daysToSimulate,
-                                       int(self.dialog.newCardsPerDaySpinbox.value()),
-                                       startingEase, len(learningSteps), len(lapseSteps), includeOverdueCards)  
+            dateArray = self.loadCards(
+                self.deckChooser.selectedId(),
+                daysToSimulate,
+                int(self.dialog.newCardsPerDaySpinbox.value()),
+                startingEase,
+                len(learningSteps),
+                len(lapseSteps),
+                includeOverdueCards,
+            )
         else:
             # Simulate a deck with x new cards
             dateArray = self.createSimulatedDateArray(
-                daysToSimulate, newCardsPerDay, mockedNewCards, startingEase)
+                daysToSimulate, newCardsPerDay, mockedNewCards, startingEase
+            )
 
-        sim = Simulator(dateArray, daysToSimulate, newCardsPerDay, intervalModifier, maxReviewsPerDay,
-                        learningSteps, lapseSteps, graduatingInterval, newLapseInterval, maxInterval,
-                        chanceRightUnseen, percentagesCorrectForLearningSteps,
-                        percentagesCorrectForLapseSteps, chanceRightYoung, chanceRightMature)
+        sim = Simulator(
+            dateArray,
+            daysToSimulate,
+            newCardsPerDay,
+            intervalModifier,
+            maxReviewsPerDay,
+            learningSteps,
+            lapseSteps,
+            graduatingInterval,
+            newLapseInterval,
+            maxInterval,
+            chanceRightUnseen,
+            percentagesCorrectForLearningSteps,
+            percentagesCorrectForLapseSteps,
+            chanceRightYoung,
+            chanceRightMature,
+        )
 
         thread = SimulatorThread(sim, parent=self)
         progress = SimulatorProgressDialog(maximum=len(dateArray), parent=self)
@@ -291,9 +377,13 @@ class SimulatorDialog(QDialog):
     def _on_simulation_done(self, data):
         self.numberOfSimulations += 1
         deck = self.mw.col.decks.get(self.deckChooser.selectedId())
-        simulationTitle = "{} ({})".format(self.dialog.simulationTitleTextfield.text(), deck['name'])
+        simulationTitle = "{} ({})".format(
+            self.dialog.simulationTitleTextfield.text(), deck["name"]
+        )
         self.dialog.simulationGraph.addDataSet(simulationTitle, data)
-        self.dialog.simulationTitleTextfield.setText("Simulation {}".format(self.numberOfSimulations + 1))
+        self.dialog.simulationTitleTextfield.setText(
+            "Simulation {}".format(self.numberOfSimulations + 1)
+        )
         self.dialog.clearLastSimulationButton.setEnabled(True)
 
     def _on_simulation_canceled(self):
@@ -309,7 +399,9 @@ class SimulatorDialog(QDialog):
         if self.numberOfSimulations == 0:
             self.dialog.clearLastSimulationButton.setEnabled(False)
         if not self.dialog.simulationTitleTextfield.isModified():
-            self.dialog.simulationTitleTextfield.setText("Simulation {}".format(self.numberOfSimulations + 1))
+            self.dialog.simulationTitleTextfield.setText(
+                "Simulation {}".format(self.numberOfSimulations + 1)
+            )
 
 
 class SimulatorThread(QThread):
@@ -360,7 +452,7 @@ def on_deck_browser_will_show_options_menu(menu, deck_id):
 
 # Main menu
 
-action = QAction('Anki Simulator', aqt.mw)
+action = QAction("Anki Simulator", aqt.mw)
 action.triggered.connect(open_simulator_dialog)
 aqt.mw.form.menuTools.addAction(action)
 
