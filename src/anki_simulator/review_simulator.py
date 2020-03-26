eaebded906c5a5a25d99816fe95107eacbf07d6a
Glutanimate <glutanimate@users.noreply.github.com>
2020-03-26 14:49:16 +0100

    Refactor dateArray updating

--- src/anki_simulator/review_simulator.py
+++ src/anki_simulator/review_simulator.py
@@ -92,16 +92,19 @@ class ReviewSimulator:
 
     def simulate(self, controller=None) -> Optional[List[dict]]:
         dayIndex = 0
+
         while dayIndex < len(self.dateArray):
 
             if controller:
                 controller.day_processed(dayIndex)
 
             reviewNumber = 0
+            daysToAdd = None
             idsDoneToday: List[int] = []
             # some cards may be postponed to the next day. We need to remove them from
             # the current day:
             removeList = []
+
             while reviewNumber < len(self.dateArray[dayIndex]):
                 if controller and controller.do_cancel:
                     return None
@@ -135,8 +138,6 @@ class ReviewSimulator:
                             card.state = CARD_STATE_LEARNING
                             card.step = card.step + 1
                             daysToAdd = int(self.learningSteps[card.step] / 1440)
-                            if (dayIndex + daysToAdd) < self.daysToSimulate:
-                                self.dateArray[dayIndex + daysToAdd].append(card)
                         else:
                             # Learning card was correct and will become a young/mature card.
                             card.ivl = self.graduatingInterval
@@ -145,16 +146,12 @@ class ReviewSimulator:
                             else:
                                 card.state = CARD_STATE_YOUNG
                             daysToAdd = card.ivl
-                            if (dayIndex + daysToAdd) < self.daysToSimulate:
-                                self.dateArray[dayIndex + daysToAdd].append(card)
                     elif card.state == CARD_STATE_RELEARN:
                         if card.step < len(self.lapseSteps) - 1:
                             # Relearn card was correct and will remain a relearn card.
                             card.state = CARD_STATE_RELEARN
                             card.step = card.step + 1
                             daysToAdd = int(self.lapseSteps[card.step] / 1440)
-                            if (dayIndex + daysToAdd) < self.daysToSimulate:
-                                self.dateArray[dayIndex + daysToAdd].append(card)
                         else:
                             # Relearn card was correct and will become a young/mature card.
                             if card.ivl >= 21:
@@ -162,8 +159,6 @@ class ReviewSimulator:
                             else:
                                 card.state = CARD_STATE_YOUNG
                             daysToAdd = card.ivl
-                            if (dayIndex + daysToAdd) < self.daysToSimulate:
-                                self.dateArray[dayIndex + daysToAdd].append(card)
                     elif card.state == CARD_STATE_YOUNG:
                         # Young card was correct and might become a mature card.
                         card.ivl = self.nextRevInterval(card.ivl, card.delay, card.ease)
@@ -171,15 +166,11 @@ class ReviewSimulator:
                         if card.ivl >= 21:
                             card.state = CARD_STATE_MATURE
                         daysToAdd = card.ivl
-                        if (dayIndex + daysToAdd) < self.daysToSimulate:
-                            self.dateArray[dayIndex + daysToAdd].append(card)
                     elif card.state == CARD_STATE_MATURE:
                         # Mature card was correct and will remain a mature card.
                         card.ivl = self.nextRevInterval(card.ivl, card.delay, card.ease)
                         card.delay = 0
                         daysToAdd = card.ivl
-                        if (dayIndex + daysToAdd) < self.daysToSimulate:
-                            self.dateArray[dayIndex + daysToAdd].append(card)
                 else:
                     if (
                         card.state == CARD_STATE_NEW
@@ -189,15 +180,11 @@ class ReviewSimulator:
                         card.state = CARD_STATE_LEARNING
                         card.step = 0
                         daysToAdd = int(self.learningSteps[0] / 1440)
-                        if (dayIndex + daysToAdd) < self.daysToSimulate:
-                            self.dateArray[dayIndex + daysToAdd].append(card)
                     elif card.state == CARD_STATE_RELEARN:
                         # Relearn card was incorrect and will remain a relearn card.
                         card.state = CARD_STATE_RELEARN
                         card.step = 0
                         daysToAdd = int(self.lapseSteps[0] / 1440)
-                        if (dayIndex + daysToAdd) < self.daysToSimulate:
-                            self.dateArray[dayIndex + daysToAdd].append(card)
                     elif (
                         card.state == CARD_STATE_YOUNG
                         or card.state == CARD_STATE_MATURE
@@ -212,14 +199,23 @@ class ReviewSimulator:
                         )  # 1 is the minimum interval
                         card.ivl = newInterval
                         daysToAdd = int(self.lapseSteps[0] / 1440)
-                        if (dayIndex + daysToAdd) < self.daysToSimulate:
-                            self.dateArray[dayIndex + daysToAdd].append(card)
+
+                if (
+                    daysToAdd is not None
+                    and (dayIndex + daysToAdd) < self.daysToSimulate
+                ):
+                    self.dateArray[dayIndex + daysToAdd].append(card)
+
                 reviewNumber += 1
+
             # We will now remove all postponed reviews from their original day:
             for index in sorted(removeList, reverse=True):
                 del self.dateArray[dayIndex][index]
+
             dayIndex += 1
+
         today = date.today()
+
         return [
             dict(x=(today + timedelta(days=index)), y=len(reviews))
             for index, reviews in enumerate(self.dateArray)
