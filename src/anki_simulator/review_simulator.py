0c156e5d635a88dfbff6b2d78a439c13395e3e3f
GiovanniHenriksen <giovannihenriksen@users.noreply.github.com>
2020-03-30 13:33:50 +0200

    Overhaul simulator (Under the hood changes)
    - Adds support for hard and easy intervals in simulator (not yet integrated in UI)
    - Scheduler fix for relearn cards that are answered 'incorrect': Ivl is additionally decreased by new lapse interval
    - Takes into account some scheduling changes in V2: Hard interval for young/mature cards is calculated a bit differently. (Still no support for hard, easy buttons in new/learning/relearn cards)
    - Added function 'adjustedIvl' which can be used to easily modify intervals/days to add. This function is now blank, but can be used to easily implement additional review schedules in the future.

--- src/anki_simulator/review_simulator.py
+++ src/anki_simulator/review_simulator.py
@@ -30,6 +30,20 @@ from .collection_simulator import (
     CARD_STATES_TYPE,
 )
 
+try:
+    from typing import Literal, Final
+except ImportError:
+    from typing_extensions import Literal, Final
+
+ANSWER_WRONG: Final = 0
+ANSWER_HARD: Final = 1
+ANSWER_GOOD: Final = 2
+ANSWER_EASY: Final = 3
+
+REVIEW_ANSWER = Literal[
+    0, 1, 2, 3,
+]
+
 
 class ReviewSimulator:
     def __init__(
@@ -46,8 +60,9 @@ class ReviewSimulator:
         max_interval: int,
         percentages_correct_for_learning_steps: List[int],
         percentages_correct_for_lapse_steps: List[int],
-        chance_right_young: int,
-        chance_right_mature: int,
+        chance_good_young: int,
+        chance_good_mature: int,
+        scheduler_version: int,
     ):
         self.dateArray: DATE_ARRAY_TYPE = date_array
         self.daysToSimulate: int = days_to_simulate
@@ -59,35 +74,108 @@ class ReviewSimulator:
         self.graduatingInterval: int = graduating_interval
         self.newLapseInterval: int = new_lapse_interval
         self.maxInterval: int = max_interval
+        self.schedulerVersion: int = scheduler_version
+        self._chance_hard: Dict[CARD_STATES_TYPE, Union[int, List[int]]] = {
+            CARD_STATE_NEW: 0,
+            CARD_STATE_LEARNING: 0,
+            CARD_STATE_RELEARN: 0,
+            CARD_STATE_YOUNG: 0,
+            CARD_STATE_MATURE: 0,
+        }
 
-        self._chance_right: Dict[CARD_STATES_TYPE, Union[int, List[int]]] = {
+        self._chance_good: Dict[CARD_STATES_TYPE, Union[int, List[int]]] = {
             CARD_STATE_NEW: percentages_correct_for_learning_steps,
             CARD_STATE_LEARNING: percentages_correct_for_learning_steps,
             CARD_STATE_RELEARN: percentages_correct_for_lapse_steps,
-            CARD_STATE_YOUNG: chance_right_young,
-            CARD_STATE_MATURE: chance_right_mature,
+            CARD_STATE_YOUNG: chance_good_young,
+            CARD_STATE_MATURE: chance_good_mature,
         }
 
-    def reviewCorrect(self, state: CARD_STATES_TYPE, step: int) -> bool:
+        self._chance_easy: Dict[CARD_STATES_TYPE, Union[int, List[int]]] = {
+            CARD_STATE_NEW: 0,
+            CARD_STATE_LEARNING: 0,
+            CARD_STATE_RELEARN: 0,
+            CARD_STATE_YOUNG: 0,
+            CARD_STATE_MATURE: 0,
+        }
+
+    def reviewAnswer(self, state: CARD_STATES_TYPE, step: int) -> REVIEW_ANSWER:
+
         randNumber = randint(1, 100)
-        chance_right = self._chance_right[state]
-        if isinstance(chance_right, (list, tuple)):
-            chance_right = chance_right[step]
-        return not (randNumber <= 100 - chance_right * 100)
+        chance_hard = self._chance_hard[state]
+        if isinstance(chance_hard, (list, tuple)):
+            chance_right = chance_hard[step]
+        chance_good = self._chance_good[state]
+        if isinstance(chance_good, (list, tuple)):
+            chance_good = chance_good[step]
+        chance_easy = self._chance_easy[state]
+        if isinstance(chance_easy, (list, tuple)):
+            chance_easy = chance_easy[step]
+        chance_incorrect = 1 - chance_hard - chance_good - chance_easy
+        if chance_incorrect < 0:
+            print(
+                "Type: {}, step: {}, random number: {}, chanceIncorrect: {}".format(
+                    state, step, randNumber, chance_incorrect
+                )
+            )
+            return (
+                -1
+            )  # chance hard + chance good + chance easy was more than 100. Returning -1 on every review
+            # to be able to catch this early, should it ever occur.
+        all_chances = [chance_incorrect, chance_hard, chance_good, chance_easy]
+        chanceSum = 0.0
+        for index, chance in enumerate(all_chances):
+            chanceSum += chance
+            if chanceSum * 100 > randNumber:
+                print(
+                    "Type: {}, step: {}, random number: {}, chanceIncorrect: {}".format(
+                        state, step, randNumber, chance_incorrect
+                    )
+                )
+                return index
+        print(
+            "Type: {}, step: {}, random number: {}, chanceIncorrect: {}".format(
+                state, step, randNumber, chance_incorrect
+            )
+        )
+        return -1
 
     def nextRevInterval(
-        self, current_interval: int, delay: int, ease_factor: int
+        self,
+        current_interval: int,
+        delay: int,
+        ease_factor: int,
+        review_answer: REVIEW_ANSWER,
     ) -> int:
-        baseHardInterval = (current_interval + delay // 4) * 1.2
+        if self.schedulerVersion == 1:
+            baseHardInterval = (current_interval + delay // 4) * 1.2
+            # Hard factor is 1.2 by default in all simulations and can't be changed.
+            # The user may have different actual settings.
+        else:
+            baseHardInterval = current_interval * 1.2
         constrainedHardInterval = max(
             baseHardInterval * self.intervalModifier, current_interval + 1
-        )  # Hard interval needs
-        # to be calculated to determine 'Normal' Interval, because new interval can not be lower than the hard interval
+        )
         baseGoodInterval = (current_interval + delay // 2) * (ease_factor / 100)
         constrainedGoodInterval = max(
             baseGoodInterval * self.intervalModifier, constrainedHardInterval + 1
         )
-        return int(min(constrainedGoodInterval, self.maxInterval))
+        baseEasyInterval = (current_interval + delay) * ease_factor * 1.5
+        # Hard factor is 1.5 by default in all simulations and can't be changed.
+        # The user may have different actual settings.
+        constrainedEasyInterval = max(
+            baseEasyInterval * self.intervalModifier, constrainedGoodInterval + 1
+        )
+        if review_answer == ANSWER_HARD:
+            return int(min(constrainedHardInterval, self.maxInterval))
+        elif review_answer == ANSWER_GOOD:
+            return int(min(constrainedGoodInterval, self.maxInterval))
+        elif review_answer == ANSWER_EASY:
+            return int(min(constrainedEasyInterval, self.maxInterval))
+
+    def adjustedIvl(self, state: CARD_STATES_TYPE, current_day: int, ideal_interval: int):
+        # This function is blank for now, but can be used to apply additional review schedules (load balancer, free weekend, etc)
+        return ideal_interval
 
     def simulate(self, controller=None) -> Optional[List[Dict[str, Union[str, int]]]]:
         dayIndex = 0
@@ -125,79 +213,141 @@ class ReviewSimulator:
                         continue
                     idsDoneToday.append(card.id)
 
-                reviewCorrect = self.reviewCorrect(card.state, card.step)
-
-                if reviewCorrect:
-                    if (
-                        card.state == CARD_STATE_NEW
-                        or card.state == CARD_STATE_LEARNING
-                    ):
+                review_answer = self.reviewAnswer(card.state, card.step)
+                if card.state == CARD_STATE_NEW:
+                    if review_answer == ANSWER_WRONG:
+                        # New card was incorrect and will become/remain a learning card.
+                        card.state = CARD_STATE_LEARNING
+                        card.step = 0
+                        daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.learningSteps[0] / 1440))
+                    elif review_answer == ANSWER_HARD:
+                        print(
+                            "No support currently for 'hard' new cards. Ignoring this card."
+                        )
+                    elif review_answer == ANSWER_GOOD:
                         if card.step < len(self.learningSteps) - 1:
-                            # Unseen/learning card was correct and will become/remain a learning card.
+                            # Unseen card was correct and will become a learning card.
                             card.state = CARD_STATE_LEARNING
                             card.step = card.step + 1
-                            daysToAdd = int(self.learningSteps[card.step] / 1440)
+                            daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.learningSteps[card.step] / 1440))
                         else:
-                            # Learning card was correct and will become a young/mature card.
-                            card.ivl = self.graduatingInterval
+                            # There are no learning steps. Unseen card was correct and will become a young/mature card.
+                            card.ivl = self.adjustedIvl(card.state, dayIndex, self.graduatingInterval)
                             if self.graduatingInterval >= 21:
                                 card.state = CARD_STATE_MATURE
                             else:
                                 card.state = CARD_STATE_YOUNG
                             daysToAdd = card.ivl
-                    elif card.state == CARD_STATE_RELEARN:
+                    elif review_answer == ANSWER_EASY:
+                        print(
+                            "No support currently for 'easy' new cards. Ignoring this card."
+                        )
+                elif card.state == CARD_STATE_LEARNING:
+                    if review_answer == ANSWER_WRONG:
+                        # Learning card was incorrect and will become/remain a learning card.
+                        card.state = CARD_STATE_LEARNING
+                        card.step = 0
+                        daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.learningSteps[0] / 1440))
+                    elif review_answer == ANSWER_HARD:
+                        print(
+                            "No support currently for 'hard' learning cards. Ignoring this card."
+                        )
+                    elif review_answer == ANSWER_GOOD:
+                        if card.step < len(self.learningSteps) - 1:
+                            # Learning card was correct and will remain a learning card.
+                            card.state = CARD_STATE_LEARNING
+                            card.step = card.step + 1
+                            daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.learningSteps[card.step] / 1440))
+                        else:
+                            # There are no learning steps left. Learning card was correct and will become a
+                            # young/mature card.
+                            card.ivl = self.adjustedIvl(card.state, dayIndex, self.graduatingInterval)
+                            if self.graduatingInterval >= 21:
+                                card.state = CARD_STATE_MATURE
+                            else:
+                                card.state = CARD_STATE_YOUNG
+                            daysToAdd = card.ivl
+                    elif review_answer == ANSWER_EASY:
+                        print(
+                            "No support currently for 'easy' learning cards. Ignoring this card."
+                        )
+                elif card.state == CARD_STATE_RELEARN:
+                    if review_answer == ANSWER_WRONG:
+                        # Relearn card was incorrect and will remain a relearn card.
+                        card.state = CARD_STATE_RELEARN
+                        card.step = 0
+                        card.ivl = max(
+                            int(card.ivl * self.newLapseInterval), 1
+                        )  # 1 is the minimum interval
+                        daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.lapseSteps[0] / 1440))
+                    elif review_answer == ANSWER_HARD:
+                        print(
+                            "No support currently for 'hard' relearn cards. Ignoring this card."
+                        )
+                    elif review_answer == ANSWER_GOOD:
                         if card.step < len(self.lapseSteps) - 1:
                             # Relearn card was correct and will remain a relearn card.
                             card.state = CARD_STATE_RELEARN
                             card.step = card.step + 1
-                            daysToAdd = int(self.lapseSteps[card.step] / 1440)
+                            daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.lapseSteps[card.step] / 1440))
                         else:
                             # Relearn card was correct and will become a young/mature card.
+                            card.ivl = self.adjustedIvl(CARD_STATE_YOUNG, dayIndex, card.ivl)
                             if card.ivl >= 21:
                                 card.state = CARD_STATE_MATURE
                             else:
                                 card.state = CARD_STATE_YOUNG
                             daysToAdd = card.ivl
-                    elif card.state == CARD_STATE_YOUNG:
-                        # Young card was correct and might become a mature card.
-                        card.ivl = self.nextRevInterval(card.ivl, card.delay, card.ease)
-                        card.delay = 0
-                        if card.ivl >= 21:
-                            card.state = CARD_STATE_MATURE
-                        daysToAdd = card.ivl
-                    elif card.state == CARD_STATE_MATURE:
-                        # Mature card was correct and will remain a mature card.
-                        card.ivl = self.nextRevInterval(card.ivl, card.delay, card.ease)
-                        card.delay = 0
-                        daysToAdd = card.ivl
-                else:
-                    if (
-                        card.state == CARD_STATE_NEW
-                        or card.state == CARD_STATE_LEARNING
-                    ):
-                        # New/learning card was incorrect and will become/remain a learning card.
-                        card.state = CARD_STATE_LEARNING
-                        card.step = 0
-                        daysToAdd = int(self.learningSteps[0] / 1440)
-                    elif card.state == CARD_STATE_RELEARN:
-                        # Relearn card was incorrect and will remain a relearn card.
-                        card.state = CARD_STATE_RELEARN
-                        card.step = 0
-                        daysToAdd = int(self.lapseSteps[0] / 1440)
-                    elif (
-                        card.state == CARD_STATE_YOUNG
-                        or card.state == CARD_STATE_MATURE
-                    ):
-                        # Young/mature card was incorrect and will become a relearn card.
+                    elif review_answer == ANSWER_EASY:
+                        print(
+                            "No support currently for 'easy' relearn cards. Ignoring this card."
+                        )
+                elif card.state == CARD_STATE_YOUNG or card.state == CARD_STATE_MATURE:
+                    if review_answer == ANSWER_WRONG:
                         card.state = CARD_STATE_RELEARN
                         card.step = 0
                         card.delay = 0
                         card.ease = max(card.ease - 20, 130)
-                        newInterval = max(
-                            int(card.ivl * self.newLapseInterval), 1
-                        )  # 1 is the minimum interval
-                        card.ivl = newInterval
-                        daysToAdd = int(self.lapseSteps[0] / 1440)
+                        card.ivl = max(int(card.ivl * self.newLapseInterval), 1)
+                        daysToAdd = self.adjustedIvl(card.state, dayIndex, int(self.lapseSteps[0] / 1440))
+                    elif review_answer == ANSWER_HARD:
+                        idealInterval = self.nextRevInterval(
+                            card.ivl, card.delay, card.ease, ANSWER_HARD
+                        )
+                        adjustedInterval = self.adjustedIvl(card.state, dayIndex, idealInterval)
+                        card.ivl = min(
+                            max(adjustedInterval, card.ivl + 1), self.maxInterval
+                        )
+                        card.delay = 0
+                        card.ease = card.ease - 15
+                        if card.ivl >= 21:
+                            card.state = CARD_STATE_MATURE
+                        daysToAdd = card.ivl
+                    elif review_answer == ANSWER_GOOD:
+                        idealInterval = self.nextRevInterval(
+                            card.ivl, card.delay, card.ease, ANSWER_GOOD
+                        )
+                        adjustedInterval = self.adjustedIvl(card.state, dayIndex, idealInterval)
+                        card.ivl = min(
+                            max(adjustedInterval, card.ivl + 1), self.maxInterval
+                        )
+                        card.delay = 0
+                        if card.ivl >= 21:
+                            card.state = CARD_STATE_MATURE
+                        daysToAdd = card.ivl
+                    elif review_answer == ANSWER_EASY:
+                        idealInterval = self.nextRevInterval(
+                            card.ivl, card.delay, card.ease, ANSWER_EASY
+                        )
+                        adjustedInterval = self.adjustedIvl(card.state, dayIndex, idealInterval)
+                        card.ivl = min(
+                            max(adjustedInterval, card.ivl + 1), self.maxInterval
+                        )
+                        card.delay = 0
+                        card.ease = card.ease + 15
+                        if card.ivl >= 21:
+                            card.state = CARD_STATE_MATURE
+                        daysToAdd = card.ivl
 
                 if (
                     daysToAdd is not None
